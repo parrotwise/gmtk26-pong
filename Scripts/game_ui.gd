c02e9c3f8e240e6536305da20e0eb9aa97f8609a
Parrotwise <parrotwisearts@gmail.com>
2026-07-18 10:00:18 +0300

    Add preliminary game scene

---
+++ Scripts/game_ui.gd
@@ -0,0 +1,10 @@
+class_name GameUI
+extends Control
+
+
+func _ready() -> void:
+	$QuitButton.pressed.connect(_on_quit_button_pressed)
+
+
+func _on_quit_button_pressed() -> void:
+	get_tree().quit()
