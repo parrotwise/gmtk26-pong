04b11a04f36696570f81d774b849c95ddb3b8ccd
Agecaf <ageof.cafeine@gmail.com>
2026-07-18 21:32:32 +0100

    Main Menu -> Game -> Main Menu, Victory and Defeat

--- Menu/main_menu.gd
+++ Menu/main_menu.gd
@@ -4,22 +4,49 @@ class_name MainMenu extends Node2D
 @onready var game_scene: GameScene = %GameScene
 @onready var play_button: Button = %Play
 @onready var quit_button: Button = %Quit
+@onready var victory_defeat_label: Label = %VictoryOrDefeatLabel
 
 # Initialization
 func _ready() -> void:
 	# Game scene doesn't start until play is pressed
 	game_scene.hide()
-	game_scene.set_process(false)
+	get_tree().paused = true
 	
 	# Signals
 	play_button.pressed.connect(_on_play)
 	quit_button.pressed.connect(_on_quit)
+	
+	# Wait for everything to be registered
+	_ready_deferred.call_deferred()
+
+func _ready_deferred():
+	GameManager.player.no_health.connect(_game_end)
+	GameManager.opponent.no_health.connect(_game_end)
+	
 
 # Go to game scene
 func _on_play() -> void:
 	game_scene.show()
-	game_scene.set_process(true)
+	GameManager.reset()
+	get_tree().paused = false
 	menu.hide()
+
+# Return to main menu on game end.
+func _game_end() -> void:
+	# Show victory or defeat text
+	if GameManager.opponent.health == 0:
+		victory_defeat_label.text = "Victory"
+	else:
+		victory_defeat_label.text = "Defeat"
+	get_tree().paused = true
+	
+	# Wait a bit
+	await get_tree().create_timer(1.0).timeout
+	
+	# Return to main menu
+	victory_defeat_label.text = ""
+	game_scene.hide()
+	menu.show()
 
 # Quit game
 func _on_quit() -> void:
--- Scripts/game_manager.gd
+++ Scripts/game_manager.gd
@@ -6,6 +6,13 @@ var ball: Ball
 var player: Player
 var opponent: Opponent
 
+# Reset the state to the start of the game
+func reset() -> void:
+	# TODO: Payer and opponent positions?
+	GameManager.player.health = 3
+	GameManager.opponent.health = 3
+	GameManager.ball.reset()
+	GameManager.hud._update_hearts()
 
 func _on_win_game() -> void:
 	Debug.info('And there was much rejoicing.')
