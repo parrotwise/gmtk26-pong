fe1ba0aa89d9610b5783e728394820ba78998d20
Parrotwise <parrotwisearts@gmail.com>
2026-07-18 19:03:41 +0300

    Implement various Ball signals, tweak the Ball's initial direction, disable processing on the Ball on scoring and reset after a small delay

--- Scenes/game_scene.gd
+++ Scenes/game_scene.gd
@@ -1,1 +1,9 @@
 class_name GameScene extends Control
+
+
+func _ready() -> void:
+	# Placeholder: Quietly reset the board so we can keep experimenting
+	GameManager.ball.score.connect(
+		func() -> void:
+			get_tree().create_timer(2).timeout.connect(GameManager.ball.reset)
+	)
--- Scripts/ball.gd
+++ Scripts/ball.gd
@@ -2,9 +2,21 @@ class_name Ball
 extends Area2D
 
 
+signal bounce
+signal bounce_wall
+signal bounce_player
+signal bounce_opponent
+signal score
+signal score_player
+signal score_opponent
+
 @export var speed: float = 250
 @export var pct_speedup_per_bump: int = 2
 
+var position_x_min: float:
+	get: return 0
+var position_x_max: float:
+	get: return get_viewport().size.x
 var position_y_min: float:
 	get: return GameManager.ui.top_bar.size.y
 var position_y_max: float:
@@ -16,9 +28,11 @@ var direction: float # in radians
 
 
 func _ready() -> void:
-	direction = RandUtil.randfloat(PI, 3*PI/2)
-
 	body_entered.connect(_on_body_entered)
+
+	GameManager.ball = self
+	
+	reset()
 
 
 func _physics_process(delta: float) -> void:
@@ -30,8 +44,36 @@ func _physics_process(delta: float) -> void:
 	if collision_top or collision_bottom:
 		position = Vector2(position.x, clampf(position.y, position_y_min, position_y_max))
 		direction = 2*PI - direction
+		bounce_wall.emit()
+	
+	var collision_left: bool = (position.x + collider_radius) <= position_x_min
+	var collision_right: bool = (position.x - collider_radius) >= position_x_max
+	
+	if collision_left:
+		set_physics_process(false)
+		score.emit()
+		score_opponent.emit()
+	
+	elif collision_right:
+		set_physics_process(false)
+		score.emit()
+		score_player.emit()
 
 
-func _on_body_entered(_body: Node2D):
+func _on_body_entered(body: Node2D) -> void:
+	if body is Player:
+		bounce.emit()
+		bounce_player.emit()
+	
+	elif body is Opponent:
+		bounce.emit()
+		bounce_opponent.emit()
+	
 	direction = PI - direction
 	speed *= 1 + pct_speedup_per_bump / 100.0
+
+
+func reset() -> void:
+	position = get_viewport().size / 2
+	direction = RandUtil.randfloat(PI * 3/4, PI * 5/4)
+	set_physics_process(true)
