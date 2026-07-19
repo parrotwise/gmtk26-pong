cfe75249d9b646bc445efe10aeac7419648df174
Parrotwise <parrotwisearts@gmail.com>
2026-07-19 21:47:37 +0300

    Reset Ball speedup on starting a new game

--- Scripts/game_manager.gd
+++ Scripts/game_manager.gd
@@ -1,26 +1,29 @@
 extends Node
 
+
 var hud: HUD
 var ball: Ball
 var player: Player
 var opponent: Opponent
 var cool_text: CoolText
 
-# Reset the state to the start of the game
+
 func reset() -> void:
-	# TODO: Payer and opponent positions?
 	player.reset()
 	opponent.reset()
 	ball.reset()
+	ball.intensity = 1
 	hud.reset()
 	cool_text.reset()
-	
+
+
 func _on_win_game() -> void:
 	Debug.info('And there was much rejoicing.')
 
 
 func _on_lose_game() -> void:
 	Debug.info('Perhaps next time. ¯\\_(ツ)_/¯')
+
 
 func _on_score() -> void:
 	# Placeholder: Quietly reset the board so we can keep experimenting
