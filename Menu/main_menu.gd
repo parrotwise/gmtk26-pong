05d8a8661fc1fd0863af380091af33f26e8aaffe
Agecaf <ageof.cafeine@gmail.com>
2026-07-19 16:42:57 +0100

    Added volume sliders

--- Menu/main_menu.gd
+++ Menu/main_menu.gd
@@ -4,6 +4,8 @@ class_name MainMenu extends Node2D
 @onready var game_scene: GameScene = %GameScene
 @onready var play_button: Button = %Play
 @onready var victory_defeat_label: Label = %VictoryOrDefeatLabel
+@onready var music_slider: HSlider = %MusicSlider
+@onready var sfx_slider: HSlider = %SFXSlider
 
 # Initialization
 func _ready() -> void:
@@ -13,9 +15,14 @@ func _ready() -> void:
 	
 	# Signals
 	play_button.pressed.connect(_on_play)
+	music_slider.value_changed.connect(_set_music_volume)
+	sfx_slider.value_changed.connect(_set_sfx_volume)
+	_set_music_volume(80.0)
+	_set_sfx_volume(80.0)
 	
 	# Wait for everything to be registered
 	_ready_deferred.call_deferred()
+	
 
 func _ready_deferred():
 	GameManager.player.no_health.connect(_game_end)
@@ -46,3 +53,17 @@ func _game_end() -> void:
 	victory_defeat_label.text = ""
 	game_scene.hide()
 	menu.show()
+
+# Set the Volume
+func _set_music_volume(value: float):
+	print(value)
+	AudioServer.set_bus_mute(1, value <= 5.0)
+	AudioServer.set_bus_volume_linear(1, value / 100.0)
+	
+
+func _set_sfx_volume(value: float):
+	print(value)
+	
+	AudioServer.set_bus_mute(2, value <= 5.0)
+	AudioServer.set_bus_volume_linear(2, value / 200.0)
+	
